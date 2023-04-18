8776b89c2d0d29a00d8434023c0dc5f53e830ce4
SnowCharm <619022098@qq.com>
2023-04-18 15:32:26 +0800

    :tada: initial commit

---
+++ src/main/java/cse/java2/project/Application.java
@@ -0,0 +1,13 @@
+package cse.java2.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/cse/java2/project/ApplicationTests.java
@@ -0,0 +1,13 @@
+package cse.java2.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
